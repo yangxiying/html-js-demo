8a9e1942ec537c0853bfcd4369558a13276042d4
yangxiying <yangxiying.2010@foxmail.com>
2020-04-29 18:17:29 +0800

    使用vue-cli生成，引入element-ui、axios试用

---
+++ vue-cli-demo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-link to="/hello">Go to hello-test</router-link>
+    <router-link to="/">Go to home</router-link>
+    <!-- 路由视图 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli-demo/src/components/HelloYxy.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>你好，我是 {{name}}</h1>
+    <el-radio v-model="radio" label="1">备选项1</el-radio>
+    <el-radio v-model="radio" label="2">备选项2</el-radio>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloYxy",
+  data() {
+    return {
+      name: "李四",
+      radio: '2'
+    };
+  }
+};
+</script>
+
+<style>
+</style>
